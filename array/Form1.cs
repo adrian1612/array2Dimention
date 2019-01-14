9a4bf8b0164ed923fc16c61962f9a25fcbab3087
adrian1612 <46441883+adrian1612@users.noreply.github.com>
2019-01-14 19:58:40 +0800

    repurbished

--- array/Form1.cs
+++ array/Form1.cs
@@ -16,5 +16,20 @@ namespace array
         {
             InitializeComponent();
         }
+
+        Graphics g;
+        Font myFont = new Font("Arial", 12, FontStyle.Regular);
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            g = CreateGraphics();
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    g.DrawRectangle(Pens.Black, new Rectangle(x * 64, y * 64, 64, 64));
+                    g.DrawString($"{x},{y}", myFont, Brushes.Red, new Point(x * 64, y * 64));
+                }
+            }
+        }
     }
 }
