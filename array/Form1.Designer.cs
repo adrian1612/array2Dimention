98e0f3dc7e57674b78f8db0e4a7c2891e1ce29aa
adrian1612 <46441883+adrian1612@users.noreply.github.com>
2019-01-15 13:07:30 +0800

    added pattern

--- array/Form1.Designer.cs
+++ array/Form1.Designer.cs
@@ -29,25 +29,38 @@
         private void InitializeComponent()
         {
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(423, 460);
+            this.numericUpDown1.Location = new System.Drawing.Point(427, 498);
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(66, 20);
             this.numericUpDown1.TabIndex = 0;
             this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
             // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(499, 497);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(494, 486);
+            this.ClientSize = new System.Drawing.Size(623, 523);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.numericUpDown1);
+            this.DoubleBuffered = true;
             this.Name = "Form1";
-            this.Text = "Adrian is Bored!";
+            this.Text = "Adrian is Bored Part2!";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
@@ -57,6 +70,7 @@
         #endregion
 
         private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- array/Form1.cs
+++ array/Form1.cs
@@ -15,6 +15,7 @@ namespace array
         public Form1()
         {
             InitializeComponent();
+            comboBox1.DataSource = Enum.GetValues(typeof(Pattern));
         }
 
         Graphics g;
@@ -26,16 +27,72 @@ namespace array
 
         private void numericUpDown1_ValueChanged(object sender, EventArgs e)
         {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                Draw(Pattern.Pattern1);
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                Draw(Pattern.Pattern2);
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                Draw(Pattern.Pattern3);
+            }
+            else if (comboBox1.SelectedIndex == 3)
+            {
+                Draw(Pattern.Pattern4);
+            }
+        }
+
+
+        void Draw(Pattern myPattern)
+        {
+            int myX = 1, myY = 1;
+            switch (myPattern)
+            {
+                case Pattern.Pattern1:
+                    myX = 1;
+                    myY = 1;
+                    break;
+                case Pattern.Pattern2:
+                    myX = 1;
+                    myY = 2;
+                    break;
+                case Pattern.Pattern3:
+                    myX = 2;
+                    myY = 1;
+                    break;
+                case Pattern.Pattern4:
+                    myX = 2;
+                    myY = 2;
+                    break;
+                default:
+                    break;
+            }
             g = CreateGraphics();
             g.Clear(Color.FromName("Control"));
-            for (int x = 0; x < numericUpDown1.Value; x++)
+            for (int x = 0; x < numericUpDown1.Value; x += myX)
             {
-                for (int y = 0; y < numericUpDown1.Value; y++)
+                for (int y = 0; y < numericUpDown1.Value; y += myY)
                 {
                     g.DrawRectangle(Pens.Black, new Rectangle(x * 32, y * 32, 32, 32));
                     g.DrawString($"{x},{y}", myFont, Brushes.Red, new Point(x * 32, y * 32));
                 }
             }
         }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            numericUpDown1.Value = 0;
+        }
     }
+}
+
+enum Pattern
+{
+    Pattern1,
+    Pattern2,
+    Pattern3,
+    Pattern4
 }
